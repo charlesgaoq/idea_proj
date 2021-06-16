815f0ebaf5efb3bd6c17c00a006e2190b7092e8d
chancegao <478484836@qq.com>
2021-06-16 16:35:54 +0800

    idea中创建spring boot工程，初始导入

---
+++ springbooteurekademo/src/main/java/com/gao/study/idea/springbooteurekademo/SpringbooteurekademoApplication.java
@@ -0,0 +1,13 @@
+package com.gao.study.idea.springbooteurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooteurekademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbooteurekademoApplication.class, args);
+    }
+
+}
---
+++ springbooteurekademo/src/test/java/com/gao/study/idea/springbooteurekademo/SpringbooteurekademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gao.study.idea.springbooteurekademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooteurekademoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
