f836ad54983bb07753ced91ad7e79af427d49916
chancegao <478484836@qq.com>
2021-06-16 17:20:05 +0800

    增加日志打印

--- springbooteurekademo/src/main/java/com/gao/study/idea/springbooteurekademo/SpringbooteurekademoApplication.java
+++ springbooteurekademo/src/main/java/com/gao/study/idea/springbooteurekademo/SpringbooteurekademoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbooteurekademoApplication {
 
     public static void main(String[] args) {
+        System.out.println("开始启动服务");
         SpringApplication.run(SpringbooteurekademoApplication.class, args);
+        System.out.println("服务启动成功");
     }
 
 }
