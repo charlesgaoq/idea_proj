0d09ec87b74ac96318bb7c7caadcaba1c2a067f7
chancegao <478484836@qq.com>
2021-06-16 18:06:47 +0800

    作为客户端注册到注册中心

---
+++ myeureka/src/main/java/com/gao/study/idea/spring/boot/myeureka/MyEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.gao.study.idea.spring.boot.myeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEurekaApplication.class,args);
+    }
+
+}
